190dc5e304cb3960e5deb6f4011a56ff447c0934
vmt1997 <tienvm272@thefirstone.jp>
2025-05-05 16:06:47 +0700

    hoc den bai 10 video so 11

---
+++ src/components/learning-resource/AddResource.vue
@@ -0,0 +1,84 @@
+<template>
+  <base-card>
+    <form @submit.prevent="submitData">
+      <div class="form-control">
+        <label for="title">Title</label>
+        <input id="title" name="title" type="text" ref="titleInput" />
+      </div>
+      <div class="form-control">
+        <label for="description">Description</label>
+        <textarea
+          id="description"
+          name="description"
+          rows="3"
+          type="text"
+          ref="descInput"
+        ></textarea>
+      </div>
+      <div class="form-control">
+        <label for="link">Link</label>
+        <textarea
+          id="link"
+          name="link"
+          rows="3"
+          type="url"
+          ref="linkInput"
+        ></textarea>
+      </div>
+      <div>
+        <base-button type="submit">Add Resource</base-button>
+      </div>
+    </form>
+  </base-card>
+</template>
+
+<script>
+export default {
+  inject: ['addResource'],
+  methods: {
+    submitData() {
+      const enteredTitle = this.$refs.titleInput.value
+      const enteredDescription = this.$refs.descInput.value
+      const enteredUrl = this.$refs.linkInput.value
+
+      if (
+        enteredTitle.trim() === '' ||
+        enteredDescription.trim === '' ||
+        enteredUrl === ''
+      ) {
+        return
+      }
+
+      this.addResource(enteredTitle, enteredDescription, enteredUrl)
+    },
+  },
+}
+</script>
+
+<style scoped>
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+}
+
+input:focus,
+textarea:focus {
+  outline: none;
+  border-color: #3a0061;
+  background-color: #f7ebff;
+}
+
+.form-control {
+  margin-block: 1rem;
+}
+</style>
